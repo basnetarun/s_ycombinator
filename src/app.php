d409c93d74ae9db1b58b5050492ae60982fe3bda
Arun Basnet <>
2019-02-10 19:19:05 +0100

    debugging enabled

--- src/app.php
+++ src/app.php
@@ -6,6 +6,7 @@ use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 
+
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
@@ -16,5 +17,7 @@ $app['twig'] = $app->extend('twig', function ($twig, $app) {
 
     return $twig;
 });
+
+$app['debug'] = true;
 
 return $app;
