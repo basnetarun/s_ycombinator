5fe51f772bb9da692f2f9135fc917729527526d2
Arun Basnet <>
2019-02-12 20:30:21 +0100

    set base namespace

--- src/app.php
+++ src/app.php
@@ -1,10 +1,13 @@
 <?php
+namespace SilexHackerNews;
 
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+
+
 
 
 $app = new Application();
