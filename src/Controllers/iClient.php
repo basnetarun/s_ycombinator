9b25f6888de15054513ec261a0d031500af604eb
Arun Basnet <>
2019-02-12 20:31:21 +0100

    cleanup interface definition

--- src/Controllers/iClient.php
+++ src/Controllers/iClient.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace SilexHackerNews\Controllers;
+namespace Controllers;
 
 
 
 /**
  * Interface defining the client class
+ * methods and properties fully defined in class implementation
  */
 interface iClient {
 
@@ -13,56 +14,54 @@ interface iClient {
     /**
      * @return int
      */
-    public function getMaxItem(): int;
+    function getMaxItem(): ?int;
 
     /**
      * @param int $id
      * @return Item|null
      */
-    public function getItem(int $id): ?Item;
+    function getItem(int $id): ?Item;
 
     /**
      * @param string $name
      * @return User|null
      */
-    public function getUser(string $name): ?User;
+    function getUser(string $name): ?User;
 
     /**
      * @return array
      */
-    public function getNewStories(): array;
+    function getNewStories(): array;
 
     /**
      * @return array
      */
-    public function getTopStories(): array;
+    function getTopStories(): array;
 
 
     /**
      * @return array
      */
-    public function getBestStories(): array;
+    function getBestStories(): array;
     
     /**
      * @return array
      */
-    public function getAskStories(): array;
+    function getAskStories(): array;
     
     /**
      * @return array
      */
-    public function getShowStories(): array;
-    
-    
+    function getShowStories(): array;
     
     
     /**
      * @return array
      */
-    public function getJobStories(): array;
+    function getJobStories(): array;
     
     /**
      * @return Updates|null
      */
-    public function getUpdates(): ?Updates;
+    function getUpdates(): ?Updates;
 }
