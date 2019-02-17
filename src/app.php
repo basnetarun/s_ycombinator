4ac8760779bb04a3f85e12a0245a23052a77938b
Arun Basnet <>
2019-02-17 02:37:01 +0100

    add/configure HttpCacheServiceProvider - not sure if we can make use of this

--- src/app.php
+++ src/app.php
@@ -6,7 +6,7 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
-
+use Silex\Provider\HttpCacheServiceProvider;
 
 
 
@@ -15,6 +15,9 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+$app->register(new HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => __DIR__.'/cache/'
+));
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
